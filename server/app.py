809ab1ca3e5c0ad79153bbc50567aeda22a3f9b6
lightningXadi <darkalpha621@gmail.com>
2026-04-08 12:47:52 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,4 +3,11 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
-from inference import app  # noqa: F401
+from inference import app
+import uvicorn
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
